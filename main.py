d7bfd75a9d3bd2e1df540a1ab3b1839429d6b2bf
zwang3210 <zwang3210@gatech.edu>
2022-02-19 16:53:15 -0600

    draft model skeleton, need finish data_loader part

---
+++ main.py
@@ -0,0 +1,119 @@
+import numpy as np
+import os
+import time
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+import model
+import yaml
+import argparse
+from model import Unet
+import train
+from torchvision import transforms
+
+
+parser = argparse.ArgumentParser(description='Model configuration')
+parser.add_argument('--config', default='configs\example.yaml')
+
+
+def main():
+    global args
+    args = parser.parse_args()
+    with open(args.config) as f:
+        config = yaml.load(f, yaml.SafeLoader)
+
+    t0 = time.time()
+
+    if config['use_gpu'] and torch.cuda.is_available():
+        device = torch.device("cuda")
+        torch.cuda.empty_cache()
+        print('GPU is ready!')
+    else:
+        device = torch.device('cpu')
+        print('CPU only!')
+
+   
+    # step 1: data   
+    # get data loader
+    train_data, val_data, test_data = None, None, None
+
+    train_loader, train_data = get_loader(data_dir=config['data_dir'], aux_data_dir=config['aux_data_dir'],
+                                          split='train',
+                                          maxseqlen=15, maxnuminstrs=10, maxnumlabels=20, maxnumims=5,
+                                          transform=transform, batch_size=config['batch_size'], shuffle=True,
+                                          num_workers=1, drop_last=False, max_num_samples=-1, use_lmdb=False, suff='')
+
+    val_loader, val_data = get_loader(data_dir=config['data_dir'], aux_data_dir=config['aux_data_dir'], split='val',
+                                      maxseqlen=15, maxnuminstrs=10, maxnumlabels=20, maxnumims=5,
+                                      transform=transform, batch_size=config['batch_size'], shuffle=False,
+                                      num_workers=1, drop_last=False, max_num_samples=-1, use_lmdb=False, suff='')
+
+    test_loader, test_data = get_loader(data_dir=config['data_dir'], aux_data_dir=config['aux_data_dir'], split='test',
+                                        maxseqlen=15, maxnuminstrs=10, maxnumlabels=20, maxnumims=5,
+                                        transform=transform, batch_size=config['batch_size'], shuffle=False,
+                                        num_workers=1, drop_last=False, max_num_samples=-1, use_lmdb=False, suff='')
+
+    print("Training samples: ", len(train_data))
+    print("Validation samples: ", len(val_data))
+    print("Test samples: ", len(test_data))
+
+    # step 2: define the model
+    if config['model_name'] == 'Unet':
+        model = Unet(config['n_class'])
+    else:
+        raise ValueError('Invalid model name !')
+
+
+    train_epoch = getattr(train, 'Train' + config['model_name'])()
+    model.to(device)
+
+    print('network structure is shown:\n\n{}'.format(model))
+
+    # step 3: loss and optimizer
+    optimizer = optim.Adam(model.parameters(), lr=config['lr'], weight_decay=config['weight_decay'])
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=config['lr_reduce_factor'],
+                                                     patience=config['lr_schedule_patience'], verbose=True)
+
+    epoch_train_losses, epoch_val_losses = [], []
+    per_epoch_time = []
+
+    # step 4: training
+    print('\n start training! \n')
+    time.sleep(1)
+    ei = torch.randn(config['batch_size'], config['embed_size'], 49) # image feature
+    with tqdm(range(config['max_epoch'])) as t:
+        for epoch in t:
+            t.set_description('Epoch:{}'.format(epoch))
+            start = time.time()
+            epoch_train_loss, optimizer = train_epoch.train(model, optimizer, device, train_loader, epoch)
+            epoch_val_loss = train_epoch.validate(model, device, val_loader, epoch)
+            epoch_test_loss = train_epoch.validate(model, device, test_loader, epoch)
+
+            epoch_train_losses.append(epoch_train_loss)
+            epoch_val_losses.append(epoch_val_loss)
+
+            t.set_postfix(time=time.time() - start, lr=optimizer.param_groups[0]['lr'], train_loss=epoch_train_loss,
+                          val_loss=epoch_val_loss)
+            per_epoch_time.append(time.time() - start)
+            scheduler.step(epoch_val_loss)
+
+            # Stop training if max hours exceed
+            if time.time() - t0 > config['max_time'] * 3600:
+                print('-' * 20)
+                print("Max_time for training elapsed {:.2f} hours, so stopping".format(config['max_time']))
+                break
+
+    # print training/testing information
+    time.sleep(1)
+
+    print("Convergence Time (Epochs): {:.4f}".format(epoch + 1))
+    print("TOTAL TIME TAKEN: {:.4f}s".format(time.time() - t0))
+    print("AVG TIME PER EPOCH: {:.4f}s".format(np.mean(per_epoch_time)))
+    print('Finish!')
+
+
+    # Write the results in results folder (TBD)
+
+if __name__ == '__main__':
+    main()
---
+++ train.py
@@ -0,0 +1,49 @@
+import numpy as np
+import os
+import torch
+import time
+
+"""
+functions in class for trianing model in single epoch (simplify the main.py)
+"""
+
+class Train(object):
+    def train(self, model, optimizer, device, data_loader, epoch):
+        model.train()
+        epoch_loss = 0
+        for it, (img_inputs, target) in enumerate(data_loader):
+            t1 = time.time()
+
+            img_inputs = img_inputs.to(device)
+            target = target.to(device)
+            optimizer.zero_grad()
+            predicts = model(img_inputs)
+            loss = model.loss(predicts, target)
+            loss.backward()
+            optimizer.step()
+            epoch_loss_plus = loss.detach().item()
+
+            epoch_loss = epoch_loss + epoch_loss_plus
+            t2 = time.time()
+            if (it + 1) % 10 == 0:
+                print("Epoch: {} loss: {:.2f}, time: {:.2f}s".format(epoch, epoch_loss_plus, t2 - t1))
+
+        epoch_loss /= (it + 1)
+
+        return epoch_loss, optimizer
+
+    def validate(self, model, device, data_loader, epoch):
+        model.eval()
+        epoch_test_loss = 0
+        with torch.no_grad():
+            for it, (img_inputs, target) in enumerate(data_loader):
+                img_inputs = img_inputs.to(device)
+                target = target.to(device)
+                predicts = model(img_inputs)
+                loss = model.loss(predicts, target)
+
+                epoch_test_loss += loss.detach().item()
+
+            epoch_test_loss /= (it + 1)
+
+        return epoch_test_loss
